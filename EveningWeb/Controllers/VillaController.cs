34c978ffc7ad14a7b5864c1ed012112d56f5ceb0
Louise <wddtechnologies@users.noreply.github.com>
2024-12-10 19:55:54 -0500

    Day 7 - controller for the Villa and the Create html page with post for forms and validation

---
+++ EveningWeb/Controllers/VillaController.cs
@@ -0,0 +1,39 @@
+﻿using Evening.Domain.Entities;
+using Evening.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EveningWeb.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [ HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if(obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("", "The description cannot exactly match the Name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
